16b8da997b62eac92447a58c7da83747b0a8e2c6
Heily Yohana Rios Ayala <heilyayala005@gmail.com>
2025-11-03 16:16:23 -0500

    Se corrigio la ruta

--- backend/routes/consultas.js
+++ backend/routes/consultas.js
@@ -2,7 +2,7 @@
 // backend/routes/consultas/consultas.js
 
 import express from 'express';
-import conexion from '../../db/conexion.js'; // Asegúrate que esta ruta sea correcta
+import conexion from '../db/conexion.js'; // Asegúrate que esta ruta sea correcta
 
 const router = express.Router();
 
